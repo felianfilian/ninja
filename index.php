81cc27931030de4c7ebdab7cce76c0c0e37f6067
Dennis Schmalstieg <darianbellentor@outlook.de>
2024-03-12 18:25:28 +0100

    alternative php index

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
-<?php 
-echo 'hello ninja'
-?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Test</title>
+</head>
+<body>
+    <h1><?php echo 'hello mario'?></h1>
+</body>
+</html>
