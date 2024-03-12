f833a539aead21063e0cb67efceed8968626abc1
Dennis Schmalstieg <darianbellentor@outlook.de>
2024-03-12 19:45:08 +0100

    array testing

--- index.php
+++ index.php
@@ -1,12 +1,5 @@
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP Test</title>
-</head>
-<body>
-    <h1><?php echo 'hello mario'?></h1>
-</body>
-</html>
+<?php
+$names = ['mario', 'luigi'];
+$names[] = 'bowser';
+print_r($names);
+?>
