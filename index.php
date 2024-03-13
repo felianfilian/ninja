2cdcac575c70af18705ffc6396bebb4a076a0870
Dennis Schmalstieg <darianbellentor@outlook.de>
2024-03-13 04:17:50 +0100

    array functions

--- index.php
+++ index.php
@@ -3,7 +3,11 @@ $num = 23.234;
 echo floor($num);
 
 $names = array('mario', 'luigi');
+$names02 = ['peach', 'toad'];
 array_push($names, 'bowser');
 print_r($names);
+echo count($names);
+$names03 = array_merge($names, $names02);
+print_r($names03);
 
 ?>
