8273ee7b3bde9940a1972783a0738649fe5a8ca0
Dennis Schmalstieg <darianbellentor@outlook.de>
2024-03-15 19:53:42 +0100

    create form

--- templates/body.php
+++ templates/body.php
@@ -9,5 +9,20 @@
     
     </nav>    
 
+    <section class="container grey-text">
+        <h4 class="center">Add a Pizza</h4>
+        <form class="white" action="" method="">
+            <label>Mail</label>
+            <input type="text" name="mail" placeholder="mail">
+            <label>Title</label>
+            <input type="text" name="title" placeholder="title">
+            <label>Ingredients</label>
+            <input type="text" name="ingredients" placeholder="ingredients">
+            <div class="center">
+                <input type="submit" name="submit" value="submit" class="btn brand z-depth-0">
+            </div>
+        </form>
+
+    </section>
 
 </body>
