5688b254d7bf766750cc9b28766c95ce14042b5c
Dennis Schmalstieg <darianbellentor@outlook.de>
2024-03-15 04:33:29 +0100

    set navbar

--- templates/body.php
+++ templates/body.php
@@ -2,6 +2,9 @@
     <div class="white depth-0">
         <div class="container">
             <a href="#" class="brand-logo brand-text">Ninja Pizza</a>
+            <ul id="nav-mobile" class="right hide-on-small-and-down">
+                <li><a href="#" class="btn brand z-depth-0">Add Pizza</a></li>
+            </ul>
         </div>
     
     </div>    
