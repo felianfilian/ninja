8f84bf48721f39005f400e6de20d45a184a7ecee
Dennis Schmalstieg <darianbellentor@outlook.de>
2024-03-15 04:31:14 +0100

    header setup

--- templates/body.php
+++ templates/body.php
@@ -1,4 +1,10 @@
-<body>
-    <h1>Ninja Pizza</h1>    
+<body class="grey lighten-4">
+    <div class="white depth-0">
+        <div class="container">
+            <a href="#" class="brand-logo brand-text">Ninja Pizza</a>
+        </div>
+    
+    </div>    
+
 
 </body>
