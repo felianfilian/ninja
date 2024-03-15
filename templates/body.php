bd901d8dbaefb70ca69b46144a6d4bb068f1467c
Dennis Schmalstieg <darianbellentor@outlook.de>
2024-03-15 04:45:38 +0100

    style update

--- templates/body.php
+++ templates/body.php
@@ -1,13 +1,13 @@
 <body class="grey lighten-4">
-    <div class="white depth-0">
-        <div class="container">
+    <nav class="white depth-0"> 
+    <div class="container">
             <a href="#" class="brand-logo brand-text">Ninja Pizza</a>
             <ul id="nav-mobile" class="right hide-on-small-and-down">
                 <li><a href="#" class="btn brand z-depth-0">Add Pizza</a></li>
             </ul>
         </div>
     
-    </div>    
+    </nav>    
 
 
 </body>
--- templates/header.php
+++ templates/header.php
@@ -5,4 +5,5 @@
     <title>Ninja Pizza</title>
     <!-- Compiled and minified CSS -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <link rel="stylesheet" href="./style.css">
 </head>
