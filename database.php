3651e602c97a538927a6b88d6236e366b6a18292
Dennis Schmalstieg <darianbellentor@outlook.de>
2024-03-17 22:15:22 +0100

    ingredients list

--- database.php
+++ database.php
@@ -19,8 +19,6 @@ mysqli_free_result($result);
 mysqli_close($conn);
 
 $ingredients = explode(',', $pizzas[0]['ingredients']);
-print_r($ingredients);
-
 
 ?>
 
@@ -44,7 +42,11 @@ print_r($ingredients);
                     <div class="card z-depth-0">
                         <div class="card-content center">
                             <h6><?php echo htmlspecialchars($pizza['title']); ?></h6>
-                            <div><?php echo htmlspecialchars($pizza['ingredients']); ?></div>
+                            <ul>
+                                <?php foreach(explode(',', $pizza['ingredients']) as $ing) { ?>
+                                <li><?php echo htmlspecialchars($ing) ?></li>
+                                <?php } ?>
+                            </ul>
                         </div>
                         <div class="card-action right-align">
                             <a href="#" class="brand-text">more info</a>
