dd9a0e27f91039c7ed243ef8dc70856f41a2265e
Dennis Schmalstieg <darianbellentor@outlook.de>
2024-03-17 09:25:42 +0100

    show all post data

--- add.php
+++ add.php
@@ -7,6 +7,10 @@ $ingredients = '';
 $errors = array('mail'=>'','title'=>'','ingredients'=>'');
 
 if(isset($_POST['submit'])) {
+    foreach($_POST as $post) {
+        echo $post . "<br/>";
+    }
+
     if(empty($_POST['mail'])) {
         $errors['mail'] = 'email required';
     } else {
