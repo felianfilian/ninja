cb22d35ba117757d1d5a210e521b4e72175816ca
Dennis Schmalstieg <darianbellentor@outlook.de>
2024-03-17 21:18:02 +0100

    first sql fetch

--- database.php
+++ database.php
@@ -1,5 +1,25 @@
 <?php
 
+$conn = mysqli_connect('localhost', 'mario', 'avalon', 'ninja_pizza');
+
+if(!$conn) {
+    echo "connection error";
+}
+
+// write query for all
+$sql = 'SELECT title, ingredients, id FROM pizzas';
+// make query and get result
+$result = mysqli_query($conn, $sql);
+// fetch resulting rows as an array
+$pizzas = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+// free result from memory
+mysqli_free_result($result);
+// close connection
+mysqli_close($conn);
+
+print_r($pizzas);
+
 ?>
 
 <html>
