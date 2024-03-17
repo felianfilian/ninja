bacda58a6b18975cc1c3f2f6ab0cf061467ef029
Dennis Schmalstieg <darianbellentor@outlook.de>
2024-03-17 21:46:09 +0100

    render mysql data

--- database.php
+++ database.php
@@ -7,7 +7,7 @@ if(!$conn) {
 }
 
 // write query for all
-$sql = 'SELECT title, ingredients, id FROM pizzas';
+$sql = 'SELECT title, ingredients, id FROM pizzas ORDER BY created_at';
 // make query and get result
 $result = mysqli_query($conn, $sql);
 // fetch resulting rows as an array
@@ -18,7 +18,6 @@ mysqli_free_result($result);
 // close connection
 mysqli_close($conn);
 
-print_r($pizzas);
 
 ?>
 
@@ -35,7 +34,22 @@ print_r($pizzas);
 <body class="grey lighten-4">
     <?php include('templates/navbar.php');  ?>
     <div class="container">
-        <h4>Pizza Database</h4>
+        <h4 class="grey-text center">Pizza Database</h4>
+        <div class="row">
+            <?php foreach($pizzas as $pizza) { ?>
+                <div class="col s6 md3">
+                    <div class="card z-depth-0">
+                        <div class="card-content center">
+                            <h6><?php echo htmlspecialchars($pizza['title']); ?></h6>
+                            <div><?php echo htmlspecialchars($pizza['ingredients']); ?></div>
+                        </div>
+                        <div class="card-action right-align">
+                            <a href="#" class="brand-text">more info</a>
+                        </div>
+                    </div>
+                </div>
+            <?php } ?>
+        </div>
     </div>
     <?php include('templates/footer.php');  ?>
 </body>
