2296a02a396bc6c4a52711e308e20bc80456c19b
Dennis Schmalstieg <darianbellentor@outlook.de>
2024-03-19 17:54:49 +0100

    create details page

---
+++ details.php
@@ -0,0 +1,15 @@
+<?php
+
+include('./config/db_connect.php');
+
+?>
+
+<html>
+<?php include('templates/header.php');  ?>
+
+<body class="grey lighten-4">
+    
+
+    <?php include('templates/footer.php');  ?>
+</body>
+</html>
