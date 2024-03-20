db7bba729e5e7d7826b12d21ae98e32a900475cf
Dennis Schmalstieg <darianbellentor@outlook.de>
2024-03-20 11:33:27 +0100

    tenery operator

---
+++ sandbox/index..php
@@ -0,0 +1,20 @@
+<?php 
+
+//ternary operator
+$score = 50;
+
+echo $score > 40 ? "highscore" : "low score";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Sandbox</title>
+</head>
+<body>
+    
+</body>
+</html>
