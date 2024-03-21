530016bf87a07a5c4b97e1c3654cd1426ecbc9fa
Dennis Schmalstieg <darianbellentor@outlook.de>
2024-03-21 17:48:05 +0100

    fopen function

--- sandbox/filesystem.php
+++ sandbox/filesystem.php
@@ -1,16 +1,19 @@
 <?php 
 
+// filesystem test 02
 $file = 'readme.txt';
+$handle = fopen($file, 'r');
+echo fread($handle, filesize($file));
 
-//filesystem
-if(file_exists($file)) {
-    $quotes = readfile($file);
-    copy($file, 'quotes.txt');
-    rename($file, 'mario.txt');
-    echo $quotes;
-} else {
-    echo "file not exist";
-}
+//filesystem test 01
+// if(file_exists($file)) {
+//     $quotes = readfile($file);
+//     copy($file, 'quotes.txt');
+//     rename($file, 'mario.txt');
+//     echo $quotes;
+// } else {
+//     echo "file not exist";
+// }
 
 
 
