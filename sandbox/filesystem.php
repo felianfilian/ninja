a86815786800cf9c048f96c189bc8c234114a3cb
Dennis Schmalstieg <darianbellentor@outlook.de>
2024-03-21 17:41:37 +0100

    rename file

--- sandbox/filesystem.php
+++ sandbox/filesystem.php
@@ -6,6 +6,7 @@ $file = 'readme.txt';
 if(file_exists($file)) {
     $quotes = readfile($file);
     copy($file, 'quotes.txt');
+    rename($file, 'mario.txt');
     echo $quotes;
 } else {
     echo "file not exist";
