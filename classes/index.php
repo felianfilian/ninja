b7eed65b835fa751c3cfa63288bc764d8f79a1fd
Dennis Schmalstieg <darianbellentor@outlook.de>
2024-03-22 09:43:51 +0100

    create class

---
+++ classes/index.php
@@ -0,0 +1,24 @@
+<?php 
+
+class User {
+    public $email;
+    public $name;
+
+    public function login() {
+        echo "user logged in";
+    }
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Classes Test</title>
+</head>
+<body>
+    <h1>Classes</h1>
+</body>
+</html>
